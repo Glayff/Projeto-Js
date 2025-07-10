6e958baec44b4e5b7832c23f59e76da73d5883bf
Glayff <glayffjr@outlook.com>
2025-07-10 15:11:37 -0400

    é isso ai

--- aula12/index.js
+++ aula12/index.js
@@ -1,1 +1,18 @@
+let varA = 'A'; //Quero que seja o Valor de B
+let varB = 'B'; //Quero que seja o Valor de C
+let varC = 'C'; //Quero que seja o Valor de A
 
+console.log(varA,varB,varC);
+
+//Minha resolução para o problema acima 
+/*const A = varB
+const B = varC
+const C = varA
+
+console.log(A,B,C);*/
+
+//resolução do professor
+
+[varA,varB,varC] = [varB,varC,varA];
+
+console.log(varA,varB,varC)

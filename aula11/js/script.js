0fba6a0dd9b4ffc5334414295be089cf20fbf9cf
Glayff <glayffjr@outlook.com>
2025-07-10 14:44:46 -0400

    fazendo mudanças

--- aula11/js/script.js
+++ aula11/js/script.js
@@ -2,7 +2,6 @@ let num1 = prompt('Digite um numero')
 let num2 = prompt('Digite outro numero')
 
 console.log(num1,num2);
-console.log
 
 num1 = Number(num1);
 num2 = Number(num2);
@@ -11,4 +10,4 @@ const soma = num1+num2;
 
 console.log(soma);  
 
-alert(`O resultado é ${soma}`);
+alert(`O resultado da sua conta é ${soma}`);

2d66a0ea52f85ec105725c8f963cb565a3ccf6a5
Glayff <glayffjr@outlook.com>
2025-07-14 10:34:40 -0400

    adição

---
+++ aula14/index.js
@@ -0,0 +1,43 @@
+// //IEEE - 754-2008
+
+let num1 = 0.7;
+let num2 = 0.1;
+
+num1 = ((num1 * 100)+(num2 * 100))/100;
+num1 = ((num1 * 100)+(num2 * 100))/100;
+num1 = ((num1 * 100)+(num2 * 100))/100;
+
+
+// num1 += num2; // num1 = num1 + num2 = 0.8
+// num1 += num2; // = 0.9
+// num1 += num2; // = 1.0
+// num1 += num2; // = 1.1
+// num1 += num2; // = 1.2
+// num1 += num2; // = 1.3
+// num1 += num2; // = 1.4
+// num1 += num2; // = 1.5
+// num1 += num2; // = 1.6
+// num1 += num2; // = 1.7
+// num1 += num2; // = 1.8
+// num1 += num2; // = 1.9
+// num1 += num2; // = 2.0
+
+// num1 = parseFloat(num1.toFixed(2));
+
+console.log(num1);
+console.log(Number.isInteger(num1));
+
+// num1 = num1.toFixed(2);
+
+// console.log(num1);
+// console.log(Number.isInteger(num1));
+
+// //console.log(num1+num2);
+// //console.log(num1.toString() + num2);
+// //console.log(num1.toString(2)); //ver o numero em forma binaria 
+// // console.log(num1.toFixed(2)); //arredondar, geralmente arredonda-se 2 casas decimais.
+// // console.log (Number.isInteger(num1));
+// let temp = num1 * 'Olá';
+// console.log(Number.isNaN(temp));
+
+

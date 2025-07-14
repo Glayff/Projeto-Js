a00248bed2c4bcf1ed4650cede4b82636dbf550c
Glayff <glayffjr@outlook.com>
2025-07-14 09:39:35 -0400

    comentarios resolução prof

--- aula13/script.js
+++ aula13/script.js
@@ -1,7 +1,7 @@
 
         const nome = prompt('Digite Seu Nome Completo:');
         let quantidade = (nome.length);
-        let letras = nome.split('');
+        let letras = nome.split(''); 
         const primeiraLetra = nome[0];
         const ultimaLetra = nome[nome.length -1];
         const primeiroIndice = nome.indexOf(primeiraLetra);  
@@ -23,9 +23,9 @@
         
         document.body.innerHTML += `Seu nome é: ${nome}<br />`;
         document.body.innerHTML += `Seu nome tem ${quantidade} letras <br />`;
-        document.body.innerHTML += `A segunda letra do seu nome é: ${letras[1]} <br />`;
-        document.body.innerHTML += `O primeiro índice da letra ${primeiraLetra} do seu nome é ${primeiroIndice} <br />`;
-        document.body.innerHTML += `O último índice da letra ${ultimaLetra} do seu nome é ${ultimoIndice}<br /br />`;
+        document.body.innerHTML += `A segunda letra do seu nome é: ${letras[1]} <br />`; //${nome.charArt(1), nome[1]}
+        document.body.innerHTML += `O primeiro índice da letra ${primeiraLetra} do seu nome é ${primeiroIndice} <br />`; //${nome.indexOf('a')}
+        document.body.innerHTML += `O último índice da letra ${ultimaLetra} do seu nome é ${ultimoIndice}<br /br />`;//${nome.lastIndexOf('a')}
         document.body.innerHTML += `As últimas 3 letras do seu nome são: ${ultima3} <br />`;
         document.body.innerHTML += `As palavras do seu nome são: ${palavras.join(',')}<br />`;
         document.body.innerHTML += `Seu nome com letras maiúsculas: ${nomeMaisculo} <br />`;

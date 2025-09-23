5a1d92ff6f7aad59147c4ea55ed05d7cafd99c68
glayff <glayffjr@outlook.com>
2025-09-23 11:22:30 -0400

    att

--- aula19/index.js
+++ aula19/index.js
@@ -1,1 +1,11 @@
-console.log ('Estudo/teste')
+/*
+Primitivos (imutáveis) - string, number, boolean , undefined, null 
+ */
+
+// let nome = 'Glayff'
+// nome = 'junior'
+// console.log(nome)
+
+let nome = 'Glayff';
+nome[0] = 'G'
+console.log(nome[0], nome);

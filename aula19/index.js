8dd053d6acb71ffbbf023d1909a6c70a025deaec
Glayff <glayffjr@outlook.com>
2025-12-02 10:20:34 -0400

    att

--- aula19/index.js
+++ aula19/index.js
@@ -1,11 +1,32 @@
-/*
-Primitivos (imutáveis) - string, number, boolean , undefined, null 
- */
+// /*
+// Primitivos (imutáveis) - string, number, boolean , undefined, null (bigit, symbol 
+// */
 
-// let nome = 'Glayff'
-// nome = 'junior'
-// console.log(nome)
+// // let nome = 'Glayff'
+// // nome = 'junior'
+// // console.log(nome)
 
-let nome = 'Glayff';
-nome[0] = 'G'
-console.log(nome[0], nome);
+// // let nome = 'Glayff';
+// // nome[0] = 'G'
+// // console.log(nome[0], nome);
+
+// // let a = 'A';
+// // let b = a; // Cópia
+
+// // console.log(a, b);
+
+// // a = 'Outra coisa'; // Alteração de 'a'
+
+// // console.log(a, b);  // 'b' permanece inalterado     
+
+// // Referência (mutável) - array, object, function
+
+let a = [1, 2, 3];
+let b = a; // Referência
+
+console.log(a, b);      
+a.push(4); // Alteração de 'a'
+console.log(a, b);  // 'b' reflete a alteração feita em 'a' e vice-versa
+
+b.pop(); // Alteração de 'b'
+console.log(b)
